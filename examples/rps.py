0efc1e35edf96a0de1023ee12068a5b98760f744
Kevify <kevy.lidstrom@outlook.com>
2021-12-29 18:59:49 +0100

    Updated rps.py

--- examples/rps.py
+++ examples/rps.py
@@ -8,3 +8,5 @@ if output[2] is False:
     print(f'Loser, You picked {output[0]}, AI picked {output[1]}')
 elif output[2] is True:
     print(f'Winner, You picked {output[0]}, AI picked {output[1]}')
+elif output[2] == None:
+    print(f"{output[1]}, {output[0]}")
