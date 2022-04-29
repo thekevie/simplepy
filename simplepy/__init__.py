80aaa412d657ef60dd55a8fe7339594e275803d8
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-29 08:56:15 +0200

    Update __init__.py

--- simplepy/__init__.py
+++ simplepy/__init__.py
@@ -7,5 +7,5 @@ __license__ = 'MIT'
 __version__ = '1.0.6'
 __description__ = 'A python package that can do math and have games'
 
-from . import games as games
-from . import math as math
+from .games import *
+from .math import *
