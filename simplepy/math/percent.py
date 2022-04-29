84d3c6c8b267608c50007a3f6062adc2446dcbd9
Kevie <65157770+thekevie@users.noreply.github.com>
2022-04-29 09:14:34 +0200

    Update percent.py

--- simplepy/math/percent.py
+++ simplepy/math/percent.py
@@ -2,7 +2,7 @@ class percent():
     def used(used, total, output=None):
         used_percent = (int(used)/int(total))*100
         if output == str:
-            used_percent = str(round(used_percent))
+            used_percent = str(round(used_percent)) + "%"
         if output == int:
             used_percent = int(round(used_percent))
         return used_percent
